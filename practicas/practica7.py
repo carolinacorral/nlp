f154fbf68300b969733aaff3888bf3a6bf7be648
porto-o <ismael.porto2003@gmail.com>
2024-11-18 18:37:43 -0600

    ADD: LSTM predict next word P7

--- practicas/practica7.py
+++ practicas/practica7.py
@@ -1,46 +1,113 @@
 import tokelib as tk
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
 import numpy as np
 
+def get_corpus():
+    sentences = []
+    directory = "practicas/corpus_7.txt" 
+    with open(directory, 'r', encoding='utf-8') as file:
+        sentences.append(file.read())
+    return sentences
 
-sentences = [
-    "Hola, yo soy Caro y soy sigma, ademas soy una foca",
-    "Soy Gsu, yo soy god y me gustan los pavos ademas soy una nutria",
-    "hola, yo soy ismael, y me gustan los pavos"
-]
+def pre_built(n_gram_size: int):
 
-corpus = [tk.tokenize(sentence, parser=' ') for sentence in sentences]
+    sentences = get_corpus()
 
-vocabulary = tk.get_vocabulary(corpus=corpus, is_nested=True)
+    corpus = [tk.tokenize(sentence, parser=' ') for sentence in sentences]
 
-n_grams = tk.get_n_grams(n=5, corpus=corpus)
+    vocabulary = tk.get_vocabulary(corpus=corpus, is_nested=True)
 
-print(f"Corpus: {corpus}\n")
+    n_grams = tk.get_n_grams(n=n_gram_size, corpus=corpus)
 
-print(f"Vocabulary: {vocabulary}\n")
+    print(f"Corpus: {corpus}\n")
 
-print(f'N grams {n_grams}')
+    print(f"Vocabulary: {vocabulary}\n")
 
-vocab_index = tk.get_word_index(vocabulary=vocabulary)
+    print(f'N grams {n_grams}')
 
-print(vocab_index)
+    vocab_index = tk.get_word_index(vocabulary=vocabulary)
 
-n_grams = tk.convert_ngrams_numbers(ngrams=n_grams, vocab_index=vocab_index)
+    print(vocab_index)
 
-print(n_grams)
+    n_grams = tk.convert_ngrams_numbers(ngrams=n_grams, vocab_index=vocab_index)
 
-
-### Red Neuronal Pre (Many-One)
-
-X = [ngram[:-1] for ngram in n_grams]
-Y = [ngram[-1] for ngram in n_grams]
-
-#print(X)
-print(Y)
+    print(n_grams)
 
 
-vocab_size = len(vocabulary)
+    ### Red Neuronal Pre (Many-One)
 
-X_one_hot = np.array([tk.one_hot_encode(ngram, vocab_size) for ngram in X])
-Y_one_hot = np.array([tk.one_hot_encode([y], vocab_size)[0] for y in Y]) #[0] para quitar dimensión extra
+    X = [ngram[:-1] for ngram in n_grams]
+    Y = [ngram[-1] for ngram in n_grams]
 
-print(X_one_hot)
+    #print(X)
+    print(Y)
+
+
+    vocab_size = len(vocabulary)
+
+    X_one_hot = np.array([tk.one_hot_encode(ngram, vocab_size) for ngram in X])
+    Y_one_hot = np.array([tk.one_hot_encode([y], vocab_size)[0] for y in Y]) #[0] para quitar dimensión extra
+
+    return X_one_hot, Y_one_hot, vocab_size, vocab_index
+
+def build_model(X_one_hot, Y_one_hot, vocab_size):
+    model = Sequential()
+
+    model.add(LSTM(50, input_shape=(X_one_hot.shape[1], X_one_hot.shape[2]), return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(vocab_size, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    model.fit(X_one_hot, Y_one_hot, epochs=50, batch_size = 1)
+
+    return model
+
+
+
+def predict(input, vocab_index, model):
+    # Tokenizar entrada
+    input = tk.tokenize(input, parser=' ')
+    
+    #+ Implementación palabra not in vocabulario (wordnet)
+
+    # Buscar el indice de cada palabra en el vocabulario
+    input_indices = [vocab_index[word] for word in input]
+
+    # Vectorizar
+    X_input = np.array([tk.one_hot_encode(input_indices, len(vocab_index))])
+    
+    X_input = X_input.reshape((X_input.shape[0], X_input.shape[1], len(vocab_index)))
+    
+    # Predicciones
+    predictions = model.predict(X_input)
+    
+    predicted_index = np.argmax(predictions)
+    
+    # obtener palabra por indice (probabilidad mayor)
+    predicted_word = [word for word, index in vocab_index.items() if index == predicted_index][0]
+    
+    return predicted_word
+
+base = input("Ingresa tu oración: ")
+n = len(tk.tokenize(base))
+
+X_one_hot, Y_one_hot, vocab_size, vocab_index = pre_built(n_gram_size=n)
+model = build_model(X_one_hot, Y_one_hot, vocab_size)
+
+while base != 'esc':
+    predicted_word = predict(input=base, vocab_index=vocab_index, model=model)
+
+    print("######")
+    print(f'{base}...{predicted_word}')
+
+    new_n = len(tk.tokenize(base))
+    if new_n != n:
+        print(f"**Longitud necesaria de la oración: {n} palabras")
+        X_one_hot, Y_one_hot, vocab_size, vocab_index = pre_built(n_gram_size=n)
+        model = build_model(X_one_hot, Y_one_hot, vocab_size)
+
+    print(f'Longitud previa: {n}')
+    base = input(f"Ingresa la siguiente oración: ")
+
---
+++ tasks.md
@@ -0,0 +1,3 @@
+# Tasks
+## practica7.py
+- [ ] Implementación palabra not in vocabulario (wordnet) (Line 73)
