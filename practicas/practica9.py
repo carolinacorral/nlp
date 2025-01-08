6face0de565f1dfa7b48dfd214c8a14b2cc3a58c
Ismael Porto <ismael.porto2003@gmail.com>
2025-01-08 18:31:44 +0000

    Word2vec stopwords not removing

--- practicas/practica9.py
+++ practicas/practica9.py
@@ -1,37 +1,25 @@
 import tokelib as tk
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input, Flatten
 import os
 
 def get_corpus():
-    textos = list()
+    sentences = []
+    directory = "practicas/etc/corpus_1.txt"
+    with open(directory, 'r', encoding='utf-8') as file:
+        sentences.append(file.read())
+    return sentences
 
-    for i in range(1, 4):
-        ruta_archivo = os.path.join("practicas/etc", f'corpus_{i}.txt')
-        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
-            textos = tk.append_cpp(textos, archivo.read())
-    return textos
-        
 def pre_built(n_gram_size: int):
     sentences = get_corpus()
-    
     corpus = [tk.tokenize(sentence, parser=' ') for sentence in sentences]
-
     corpus = [tk.remove_stopwords(doc) for doc in corpus]
 
     vocabulary = tk.get_vocabulary(corpus=corpus, is_nested=True)
-    
-    #print(vocabulary)
-
     n_grams = tk.get_n_grams(n=n_gram_size, corpus=corpus)
-    
     vocab_index = tk.get_word_index(vocabulary=vocabulary)
-
     n_grams = tk.convert_ngrams_numbers(ngrams=n_grams, vocab_index=vocab_index)
-
-
-    #print(n_grams)
 
     vocab_size = len(vocabulary)
     
@@ -41,10 +29,39 @@ def pre_built(n_gram_size: int):
     X_one_hot = np.array([tk.one_hot_encode(ngram, vocab_size) for ngram in X])
     Y_one_hot = np.array([tk.one_hot_encode([y], vocab_size)[0] for y in Y]) #[0] para quitar dimensión extra
 
-    return X_one_hot, Y_one_hot, vocab_size, vocab_index
+    return X_one_hot, Y_one_hot, vocab_size, vocab_index, vocabulary
 
 def build_model(X_one_hot, Y_one_hot, vocab_size):
-    pass
+    model = Sequential([
+        Input(shape=(X_one_hot.shape[1], vocab_size)),  # Ajusta la entrada al tamaño correcto
+        Flatten(),  # Asegura que la salida sea plana
+        Dense(128, activation='relu'),  # Capa oculta con 128 neuronas
+        Dense(vocab_size, activation='softmax')  # Salida ajustada al vocabulario
+    ])
 
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_one_hot, Y_one_hot, epochs=110, batch_size=256, verbose=True)
+    return model
+
+# Entrenamiento
 n = 2
-X_one_hot, Y_one_hot, vocab_size, vocab_index = pre_built(n_gram_size=n)
+X_one_hot, Y_one_hot, vocab_size, vocab_index, vocabulary = pre_built(n_gram_size=n)
+model = build_model(X_one_hot, Y_one_hot, vocab_size)
+
+##
+weights = model.get_weights()[0]
+
+word_embeddings = {}
+for word in vocabulary:
+    word_embeddings[word] = weights[vocab_index[word]]
+
+print(word_embeddings)
+
+import matplotlib.pyplot as plt
+
+# plt.figure(figsize = (10, 10))
+for word in list(vocab_index.keys()):
+    coord = word_embeddings.get(word)
+    plt.scatter(coord[0], coord[1])
+    plt.annotate(word, (coord[0], coord[1]))
+    plt.show()
--- practicas/tokelib.py
+++ practicas/tokelib.py
@@ -38,9 +38,42 @@ def is_stopword(word, english=False):
     bool: True si la palabra es una stopword en el idioma especificado, False en caso contrario.
     """
     if english == False:
-        stopwords = ["el", "y", "eso", "la", "los", "las", "de", "en", "a", "un", "uno", "una", "es", "con", "para", "al", "del", "por", "esto", "su", "sus", "esto", "ha"]
+        stopwords = [
+            "el", "y", "eso", "la", "los", "las", "de", "en", "a", "un", "uno", "una", 
+            "es", "con", "para", "al", "del", "por", "esto", "su", "sus", "esto", "ha", 
+            "las", "se", "que", "como", "o", "mismo", "si", "más", "menos", "esto", 
+            "nos", "ya", "no", "muy", "cada", "este", "esta", "estos", "estas", "mucho", 
+            "también", "entre", "cuando", "donde", "algunos", "algunas", "todo", 
+            "toda", "todos", "todas", "ser", "estar", "había", "hacia", "tener", "ni", 
+            "siempre", "nuestra", "nuestro", "nuestras", "nuestros", "sus", "esta", 
+            "esos", "esos", "esas", "quien", "quienes", "ni", "aunque", "sobre", 
+            "cual", "cuales", "quien", "cualquiera", "uno", "donde", "después", "antes",
+            "sino", "ahora", "uso", "solo", "detrás", "cómo", "se", "sin", "con", "vez",
+            "como"
+        ]
     else:
-        stopwords = ["at", "him","on","in","to","her","then", "she", "his", "the", "and", "that", "the", "this", "those", "these", "a", "an", "is", "with", "for", "of"]
+        stopwords = ['the',
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", 
+    "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", 
+    "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", 
+    "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", 
+    "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", 
+    "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", 
+    "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", 
+    "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", 
+    "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", 
+    "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", 
+    "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", 
+    "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", 
+    "the", "their", "theirs", "them", "themselves", "then", "there", "there's", 
+    "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", 
+    "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", 
+    "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", 
+    "when's", "where", "where's", "which", "while", "who", "who's", "whom", 
+    "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", 
+    "you're", "you've", "your", "yours", "yourself", "yourselves"
+]
+
     return word in stopwords
 
 def to_lower(letter):
@@ -128,6 +161,7 @@ def remove_stopwords(doc, english = False):
     """
     new_doc = []
     for word in doc:
+        print(word)
         if not is_stopword(word, english):
             new_doc.append(word)
     return new_doc
